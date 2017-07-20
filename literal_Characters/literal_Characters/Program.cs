c795c84fcf673ecafe2baa5ac4a683dda119c9d6
karimProgrammer1 <karim_nabil_2007@hotmail.com>
2017-07-21 01:47:40 +0200

    Literal Characters
    This a program to Explain Literal characters and notes for New Version in C#

---
+++ literal_Characters/literal_Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace literal_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = -100;
+            uint myUVar = 1000U;
+            
+            float myFlVar = 1.5f;
+            double myDVar = 100.5d;
+            decimal myDecVar = 100.5m;
+
+            Console.WriteLine("{0} {1}",myVar, myUVar);
+            Console.WriteLine($"{myDVar},{myDecVar}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
