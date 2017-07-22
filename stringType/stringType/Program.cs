a9ede369dbc08080811d46d218022300e3b87ee0
karimProgrammer1 <karim_nabil_2007@hotmail.com>
2017-07-22 06:03:01 +0200

    String Type
    this is a program to explain a string type

---
+++ stringType/stringType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Karim said this is a good course and I think";
+            string myString2 = "Yes, It Is";
+            string myString3 = myString + myString2;
+            Console.WriteLine("{0}", myString3);
+            Console.ReadKey();
+        }
+    }
+}
